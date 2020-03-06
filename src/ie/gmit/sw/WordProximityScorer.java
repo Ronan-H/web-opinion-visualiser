5086bc38f34770045e1428f45cc9a81b612cc852
ronan <g00342393@gmit.ie>
2020-03-06 22:00:37 +0000

    Added WordProximityScorer class

---
+++ src/ie/gmit/sw/WordProximityScorer.java
@@ -0,0 +1,15 @@
+package ie.gmit.sw;
+
+import java.util.Map;
+
+public class WordProximityScorer {
+    private String text;
+
+    public WordProximityScorer(String text) {
+        this.text = text;
+    }
+
+    public Map<String, Integer> getWordScores() {
+        return null;
+    }
+}
