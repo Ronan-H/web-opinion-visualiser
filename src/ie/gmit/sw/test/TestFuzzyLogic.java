e2d06cacbb05d682c2135c26bd167f9675991036
ronan <g00342393@gmit.ie>
2020-03-31 13:34:32 +0100

    Improved fuzzy rules and membership functions

---
+++ src/ie/gmit/sw/test/TestFuzzyLogic.java
@@ -0,0 +1,40 @@
+package ie.gmit.sw.test;
+
+import net.sourceforge.jFuzzyLogic.*;
+import net.sourceforge.jFuzzyLogic.plot.JFuzzyChart;
+import net.sourceforge.jFuzzyLogic.rule.Variable;
+
+public class TestFuzzyLogic {
+    // based on tipper example: http://jfuzzylogic.sourceforge.net/html/example_java.html
+    public static void main(String[] args) {
+        // Load from 'FCL' file
+        String fileName = "./res/page-scoring.fcl";
+        FIS fis = FIS.load(fileName,true);
+        // Error while loading?
+        if(fis == null) {
+            System.err.printf("Can't load file: '%s'%n", fileName);
+            return;
+        }
+
+        // Show
+        FunctionBlock fb = fis.getFunctionBlock("page_scoring");
+        JFuzzyChart.get().chart(fb);
+
+        // Set inputs
+        fis.setVariable("relevance", 5);
+        fis.setVariable("domain_usage", 0);
+        fis.setVariable("depth", 0);
+
+        // Evaluate
+        fis.evaluate();
+
+        // Show output variable's chart
+        Variable out = fis.getVariable("score");
+        JFuzzyChart.get().chart(out, out.getDefuzzifier(), true);
+        System.out.println("Score: " + out.getLatestDefuzzifiedValue());
+
+
+        // Print ruleSet
+        System.out.println(fis);
+    }
+}
