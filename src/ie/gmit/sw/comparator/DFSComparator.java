c8ebb3bfd0c2a6c5aadc41a612d176bdb6304786
Ronan-H <g00342393@gmit.ie>
2020-04-16 14:11:06 +0100

    Changed num search results for DFS

--- src/ie/gmit/sw/comparator/DFSComparator.java
+++ src/ie/gmit/sw/comparator/DFSComparator.java
@@ -19,6 +19,6 @@ public class DFSComparator extends PageNodeEvaluator {
     // limit DuckDuckGo search results to force a deeper search
     @Override
     public int numSearchResultsToUse() {
-        return 2;
+        return 3;
     }
 }
